2bd76cb63acc75806088aaa468cb4873be66f7d0
tutstricks <tutstricks@gmail.com>
2021-03-18 15:43:13 +0500

    done before something little

--- users/forms.py
+++ users/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import userFile#, comment
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField() #default is true
@@ -18,3 +19,16 @@ class fileForm(forms.ModelForm):
 #     class Meta:
 #         model = comment
 #         fields = ('post','body','')
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email','first_name','last_name']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
--- users/views.py
+++ users/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.core.files.storage import FileSystemStorage
-from .forms import fileForm
+from .forms import fileForm, UserUpdateForm, ProfileUpdateForm
 from .models import userFile
 from django.views.generic import ListView
 from django.contrib.auth.models import User
@@ -28,8 +28,26 @@ def register(request):
 
 @login_required()
 def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
 
-    return render(request,'users/profile.html')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request,'users/profile.html', context)
 
 class fileDetail(DetailView):
     model = userFile
